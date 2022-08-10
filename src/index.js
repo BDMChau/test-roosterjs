579683f789e561a74ba18bc8ff9772b4bdc4134a
MChau105 <bdmchau10005@gmail.com>
2022-08-10 23:59:16 +0700

    'demo'

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
